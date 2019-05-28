f4e03a4836774df88714093cddfb6eb77880dbbc
danielmulvad <dhm@hallas.nu>
2019-05-27 17:15:49 -0700

    change URL

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,10 @@ export default function Home (props) {
   const [user, setUserData] = useState()
   const [data, setData] = useState({ messages: [] })
   const [message, setMessage] = useState()
-  var ws = new WebSocket('wss://dhm.wtf:8080') // Connect to websocket
+  const ws = new WebSocket('wss://dhm.wtf:8080', {
+    origin: 'https://dhm.wtf'
+  })
+  // Connect to websocket
   useEffect(() => {
     import('../css/home.css')
     var u = window.localStorage.getItem('user')
